236987a8ef8554b6e22b628490586eafd2f57a28
paginainsta32-bit <paginainsta32@gmail.com>
2026-06-15 12:36:07 -0300

    Add files via upload

---
+++ js/despesas.js
@@ -0,0 +1,35 @@
+﻿async function salvarDespesa(){
+
+const descricao =
+document.getElementById("descricao").value;
+
+const valor =
+Number(
+document.getElementById("valor").value
+);
+
+await fetch(
+`${API_URL}/${TABLES.despesas}/?user_field_names=true`,
+{
+method:"POST",
+
+headers:{
+Authorization:`Token ${BASEROW_21WDS4cSz6izrJsEfUzRblsIdtj9DCF5}`,
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+data:new Date()
+.toISOString()
+.split("T")[0],
+
+descricao:descricao,
+
+valor:valor
+
+})
+});
+
+alert("Despesa cadastrada");
+}
---
+++ js/login.js
@@ -0,0 +1,43 @@
+﻿async function login(){
+
+const email =
+document.getElementById("email").value;
+
+const senha =
+document.getElementById("senha").value;
+
+const response = await fetch(
+`${API_URL}/${TABLES.usuarios}/?user_field_names=true`,
+{
+headers:{
+Authorization:`Token ${BASEROW_TOKEN}`
+}
+});
+
+const data = await response.json();
+
+const usuario =
+data.results.find(u =>
+u["e-mail"] === email &&
+u["senha"] === senha
+);
+
+if(usuario){
+
+localStorage.setItem(
+"usuario",
+JSON.stringify(usuario)
+);
+
+window.location =
+"dashboard.html";
+
+}else{
+
+document.getElementById("msg")
+.innerHTML =
+"Usuário ou senha inválidos";
+
+}
+
+}
