ec40a4849999b919d81915028f536503c2df1cfe
paginainsta32-bit <paginainsta32@gmail.com>
2026-06-15 13:40:46 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,54 +1,16 @@
 async function login() {
 
-    const email = document.getElementById("email").value.trim();
-    const senha = document.getElementById("senha").value.trim();
-
-    try {
-
-        const response = await fetch(
-            `${API_URL}/${TABLES.usuarios}/?user_field_names=true`,
-            {
-                headers: {
-                    Authorization: `Token ${BASEROW_TOKEN}`
-                }
+    const response = await fetch(
+        `${API_URL}/${TABLES.usuarios}/?user_field_names=true`,
+        {
+            headers: {
+                Authorization: `Token ${BASEROW_TOKEN}`
             }
-        );
-
-        const data = await response.json();
-
-        console.log("RESPOSTA BASEROW:");
-        console.log(data);
-
-        const usuario = data.results.find(u =>
-            String(u["e-mail"]).trim() === email &&
-            String(u["senha"]).trim() === senha
-        );
-
-        if (usuario) {
-
-            localStorage.setItem(
-                "usuario",
-                JSON.stringify(usuario)
-            );
-
-            alert("Login realizado!");
-
-            window.location.href = "dashboard.html";
-
-        } else {
-
-            document.getElementById("msg").innerHTML =
-                "Usuário ou senha inválidos";
-
         }
+    );
 
-    } catch (erro) {
+    const data = await response.json();
 
-        console.error(erro);
-
-        document.getElementById("msg").innerHTML =
-            "Erro ao conectar ao Baserow";
-
-    }
+    alert(JSON.stringify(data, null, 2));
 
 }
