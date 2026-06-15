1537dd2d9dfb35ae4afe9266546313ebc1cf2d87
paginainsta32-bit <paginainsta32@gmail.com>
2026-06-15 10:57:15 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿const BASEROW_TOKEN = "COLE_SEU_NOVO_TOKEN_AQUI";
+
+const API_URL =
+"https://api.baserow.io/api/database/rows/table";
+
+const TABLES = {
+    usuarios: 1028864,
+    vendas: 1028866,
+    despesas: 1028890
+};
---
+++ dashboard.js
@@ -0,0 +1,64 @@
+﻿async function carregarDashboard(){
+
+const vendasResponse =
+await fetch(
+`${API_URL}/${TABLES.vendas}/?user_field_names=true`,
+{
+headers:{
+Authorization:`Token ${BASEROW_TOKEN}`
+}
+});
+
+const despesasResponse =
+await fetch(
+`${API_URL}/${TABLES.despesas}/?user_field_names=true`,
+{
+headers:{
+Authorization:`Token ${BASEROW_TOKEN}`
+}
+});
+
+const vendas =
+await vendasResponse.json();
+
+const despesas =
+await despesasResponse.json();
+
+let totalVendas = 0;
+let totalDespesas = 0;
+
+vendas.results.forEach(v=>{
+
+totalVendas +=
+Number(v.valor_total || 0);
+
+});
+
+despesas.results.forEach(d=>{
+
+totalDespesas +=
+Number(d.valor || 0);
+
+});
+
+const lucro =
+totalVendas - totalDespesas;
+
+document.getElementById(
+"vendasTotal"
+).innerHTML =
+`R$ ${totalVendas.toFixed(2)}`;
+
+document.getElementById(
+"despesasTotal"
+).innerHTML =
+`R$ ${totalDespesas.toFixed(2)}`;
+
+document.getElementById(
+"lucroTotal"
+).innerHTML =
+`R$ ${lucro.toFixed(2)}`;
+
+}
+
+carregarDashboard();
